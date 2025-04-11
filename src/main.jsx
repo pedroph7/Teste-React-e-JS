ab8a5d7e0898d8f8344db83cf6f86a5845481312
Pedro Henrique Budziak <84034586+pedroph7@users.noreply.github.com>
2025-04-11 17:06:14 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Contact from './Contact.jsx'
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
   {
     path: "/contact",
-    element: <Contact/>
-  }
+    element: <Contact/>,
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics/>
-    <SpeedInsights/>
-   <RouterProvider router={router} />
+     <Analytics/>
+     <RouterProvider router={router} />
   </StrictMode>,
 )
